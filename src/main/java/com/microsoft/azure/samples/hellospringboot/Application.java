ec58a9244243dc72349034007e34b23126664ba7
PeaksourceGroup <contact@peaksource.tn>
2018-12-23 23:21:44 +0100

    test

--- src/main/java/com/microsoft/azure/samples/hellospringboot/Application.java
+++ src/main/java/com/microsoft/azure/samples/hellospringboot/Application.java
@@ -21,7 +21,7 @@ public class Application {
 
     @Bean
     RouterFunction<ServerResponse> routes() {
-        return route(GET("/api/hello"), request -> ok().syncBody("Hello, Azure!"));
+        return route(GET("/api/hello"), request -> ok().syncBody("Hello, people!"));
     }
 
     public static void main(String[] args) {
